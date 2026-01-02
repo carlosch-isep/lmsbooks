6f772f90c264f7980ce150a71d230cb3490f0d7b
Carlos <carloscastrohenriques@gmail.com>
2026-01-02 15:01:52 +0000

    Update smoke tests

--- load-tests/smoke/create-book-smoke.js
+++ load-tests/smoke/create-book-smoke.js
@@ -3,10 +3,24 @@ import { check, sleep } from 'k6';
 
 export let options = {
   vus: 1,
-  duration: '10s',
+  duration: '3s',
 };
 
 export default function () {
+
+  const TOKEN = 'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAiuZ4N2VZ8bo95gLg/tyV' +
+      'p6hEaR7NnXqGsPdg7iWVEnHLMEMEpxKKSRqies2xgqJYK+vqdXF5qmIc9arMsKQQ' +
+      'wHW8U3uMUtfAE5XMjgX0eUv4MrZOexJViUxgHpWo214J3tq/+hXuuoFnz514q18d' +
+      '413FW3l674+S7ISVjvrYQeI10IHfhXPG8YjXTtZl96ZeNN7Kfdn+twn/RyuBNaOE' +
+      'shvRhklk46BPFJYUQvh6W/KpA5c9csNj0QwJDqquwugM38GfSUaOYki9nBz+cYJW' +
+      'TwsA7ZjF53my606Ml1iUspkpypVSWOd9MAUjGOysLOQL//kpkIAhpKAHkKA0qTwa' +
+      'HtkrCyZLghTxHq6i89oMZFOdl2AAxz7AbB2xGPexitYc1gchGTnsKVvkMqe/IdNh' +
+      'I8T6YJAwVZ8kp2CmyMblg7+4XQORedSnY/Hdi19ljaBkkiUDOlsMcsY+2Da07O5G' +
+      'ojpVJLsjnlE4KjHNi9vdSOENS2Qlnuty2X/NziWDAzrnhsINMaOEuwwWco+8HYrv' +
+      'lqichDEK2colbDpxWNBqBpTHF4p+jtBVzIomXFTm7r5PRaPrVcP8yMF5TWs9jwUq' +
+      'EqjWnErcnmNb9F3AeocFi8DTd6x6YxXA9G8YMdx1JEKiggIPu587viYxiFEgSmC+' +
+      'DSHH49SuJzUozyddXZG0A5kCAwEAAQ==';
+
   let payload = JSON.stringify({
     isbn: "9783161484100",
     title: "The Art of Clean Code",
@@ -26,11 +40,13 @@ export default function () {
   });
 
   let params = {
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization' : 'Bearer ' + TOKEN
+    },
   };
 
   let res = http.post('http://localhost:8087/api/command/books', payload, params);
   check(res, { 'created': (r) => r.status === 201 });
   sleep(1);
 }
-
--- load-tests/smoke/get-books-smoke.js
+++ load-tests/smoke/get-books-smoke.js
@@ -2,12 +2,40 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-    duration: '10s',
-    vus: 2,
+    vus: 1,
+    duration: '1s',
 };
 
 export default function () {
-    let res = http.get('http://localhost:8088/api/query/books');
-    check(res, { 'status was 200': (r) => r.status === 200 });
+
+    const TOKEN = 'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAiuZ4N2VZ8bo95gLg/tyV' +
+        'p6hEaR7NnXqGsPdg7iWVEnHLMEMEpxKKSRqies2xgqJYK+vqdXF5qmIc9arMsKQQ' +
+        'wHW8U3uMUtfAE5XMjgX0eUv4MrZOexJViUxgHpWo214J3tq/+hXuuoFnz514q18d' +
+        '413FW3l674+S7ISVjvrYQeI10IHfhXPG8YjXTtZl96ZeNN7Kfdn+twn/RyuBNaOE' +
+        'shvRhklk46BPFJYUQvh6W/KpA5c9csNj0QwJDqquwugM38GfSUaOYki9nBz+cYJW' +
+        'TwsA7ZjF53my606Ml1iUspkpypVSWOd9MAUjGOysLOQL//kpkIAhpKAHkKA0qTwa' +
+        'HtkrCyZLghTxHq6i89oMZFOdl2AAxz7AbB2xGPexitYc1gchGTnsKVvkMqe/IdNh' +
+        'I8T6YJAwVZ8kp2CmyMblg7+4XQORedSnY/Hdi19ljaBkkiUDOlsMcsY+2Da07O5G' +
+        'ojpVJLsjnlE4KjHNi9vdSOENS2Qlnuty2X/NziWDAzrnhsINMaOEuwwWco+8HYrv' +
+        'lqichDEK2colbDpxWNBqBpTHF4p+jtBVzIomXFTm7r5PRaPrVcP8yMF5TWs9jwUq' +
+        'EqjWnErcnmNb9F3AeocFi8DTd6x6YxXA9G8YMdx1JEKiggIPu587viYxiFEgSmC+' +
+        'DSHH49SuJzUozyddXZG0A5kCAwEAAQ==';
+
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : 'Bearer ' + TOKEN
+        },
+    };
+
+    const url = 'http://localhost:8088/api/query/books?title=The%20Art%20of%20Clean%20Code';
+
+    let res = http.get(url, params);
+
+    check(res, {
+        'status was 200': (r) => r.status === 200,
+        'body size > 0': (r) => r.body.length > 0
+    });
+
     sleep(1);
 }
