3abd84aac7b8825cdee640053e72f61bc98d48c0
Carlos <carloscastrohenriques@gmail.com>
2026-01-03 21:47:29 +0000

    Add swarm init and create network

--- scripts/deployment.groovy
+++ scripts/deployment.groovy
@@ -25,6 +25,23 @@ def deploy(branch, strategy) {
         sh "${ssh} ${branch} 'cd /opt/books/${branch}/ && IMAGE_TAG=${imageTag} docker compose pull && docker compose up -d'"
     } else {
         // --- INIT CANARY VERSION
+
+        // Make sure swarm is active
+        sh """${ssh} ${branch} '
+        if ! docker info | grep -q "Swarm: active"; then
+            docker swarm init
+        fi
+        '
+        """
+
+        // Make sure network exists
+        """ ${ssh} ${branch} '
+        if ! docker network ls | grep -q "lms_overlay_attachable_network"; then
+            docker network create --driver overlay --attachable lms_overlay_attachable_network
+        fi
+        '
+        """
+
         sh "scp -o StrictHostKeyChecking=no -F ./deployment-resources/ssh_deployment_config target/LMSBooks-0.0.1-SNAPSHOT.jar ${branch}:/opt/books/${branch}/target/LMSBooks-0.0.1-SNAPSHOT.jar"
 
         // Proxy
